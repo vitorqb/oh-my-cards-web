b8f7c767db20b9472f63178583802edb6a462578
vitor quintanilha barbosa <vitorqb@gmail.com>
2020-10-18 12:18:39 +0200

    FIX Special characters broke fuzzy matching
    - Using special characters reserved for regex caused errors on fuzzy
    matching

--- src/ohmycards/web/common/string/fuzzy_match.cljs
+++ src/ohmycards/web/common/string/fuzzy_match.cljs
@@ -1,10 +1,15 @@
 (ns ohmycards.web.common.string.fuzzy-match
   (:require [clojure.string :as s]))
 
+(defn- escape-regex
+  "Escapes special chars in regexp"
+  [r]
+  (s/replace r (re-pattern "[-/\\^$*+?.()|[\\]{}]") "\\$&"))
+
 (defn main
   "Fuzzy matches given a user input string and a seqable of strings."
   [inp strings]
-  (let [regexp    (re-pattern (s/lower-case (s/replace inp #" " ".+")))
+  (let [regexp (-> inp escape-regex (s/replace #" " ".+") s/lower-case re-pattern)
         lower-inp (s/lower-case inp)]
     (for [s strings
           :let [lower-s (s/lower-case s)]
